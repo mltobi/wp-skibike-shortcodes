c0e833833c647857a12eeb0d007cca216306b25f
mltobi <65978622+mltobi@users.noreply.github.com>
2024-10-16 08:04:48 +0200

    Update skibike-shortcodes.php to fix division by zero and show only last 5 months

--- skibike-shortcodes.php
+++ skibike-shortcodes.php
@@ -10,6 +10,7 @@ function func_list_orders_of_product( $atts ) {
   $retStr = '';
   $html_th = '<td style="border:solid;padding:4px;font-size:small;font-size:0.8em;">';
   $html_td = '<td style="border:solid;padding:4px;white-space: nowrap;font-size:0.8em;">';
+  $five_months = 5 * 30 * 24 * 60 * 60; // 5 months in seconds
 
   // get all products
   $products = wc_get_products( array('numberposts' => -1) );
@@ -56,73 +57,87 @@ function func_list_orders_of_product( $atts ) {
     $retStr = $retStr . '<table>';
     $table_header = 0;
     $invoice_number = 'not found';
-
+	  
     // loop through all orders
     $order_t = 14;
     foreach( $orders as $order ) {
       $order_t = $order;
+
+	  // get order creation timestamp
+	  $date_created = $order->get_date_created();
+	  $date_created_timestamp = $date_created->getTimestamp();
+		
+	  // get current timestamp of "now"
+	  $now = new WC_DateTime(); 
+      $now->setTimezone( $date_created->getTimezone() );
+      $now_timestamp = $now->getTimestamp();
+
+		   // $retStr = $retStr . '<pre>' . print_r($date_created_timestamp, true) . '</pre>';
+		   // $retStr = $retStr . '<pre>' . print_r($now_timestamp - $five_months, true) . '</pre>';
 
       // loop through all order items of current order
       foreach ($order->get_items() as $item_key => $item_values) {
 
         // get product id of current orders
         $product_id = $item_values->get_product_id();
-
+		  
         // check if product id was selected by user
         if( $product_id == $product_selected ) {
 
           // get order data of current order
           $order_data = $order->get_data(); 
 
-          // eval invoice number of current order
-          foreach( $order->get_meta_data('_wcpdf_invoice_number_data') as $meta_data) {
-            if( $meta_data->key == '_wcpdf_invoice_number_data' ) {
-              $invoice_number = $meta_data->value['formatted_number'];
-            }
-          }
+		  if( ($now_timestamp - $five_months) < $date_created_timestamp ) 
+		  {			
+			// eval invoice number of current order
+			foreach( $order->get_meta_data('_wcpdf_invoice_number_data') as $meta_data) {
+			  if( $meta_data->key == '_wcpdf_invoice_number_data' ) {
+				$invoice_number = $meta_data->value['formatted_number'];
+			  }
+			}
 
-          // add table header
-          if( $table_header == 0 ) {
-            $table_header = 1;
-            $retStr = $retStr . '<tr style="background-color:lightgrey;">';
-            $retStr = $retStr . $html_th . 'Rechnungsnr.</th>';
-            $retStr = $retStr . $html_th . 'Anmelder</th>';
-            $retStr = $retStr . $html_th .'Menge</th>';
-            // add header for custom field data dependent columns
-            foreach( $item_values->get_meta_data('') as $meta_data) {
-              if( !str_starts_with($meta_data->key, '_') ) {
-                $retStr = $retStr . $html_th . $meta_data->key . '</th>';
-              }
-            }
-            $retStr = $retStr . $html_th . 'E-Mail</th>';
-            $retStr = $retStr . $html_th . 'Telefon</th>';
-            $retStr = $retStr . $html_th . 'Status</th>';
-            $retStr = $retStr . '</tr>';
-          }
+			// add table header
+			if( $table_header == 0 ) {
+			  $table_header = 1;
+			  $retStr = $retStr . '<tr style="background-color:lightgrey;">';
+			  $retStr = $retStr . $html_th . 'Rechnungsnr.</th>';
+			  $retStr = $retStr . $html_th . 'Anmelder</th>';
+			  $retStr = $retStr . $html_th .'Menge</th>';
+			  // add header for custom field data dependent columns
+			  foreach( $item_values->get_meta_data('') as $meta_data) {
+			    if( !str_starts_with($meta_data->key, '_') ) {
+				  $retStr = $retStr . $html_th . $meta_data->key . '</th>';
+				}
+			  }
+			  $retStr = $retStr . $html_th . 'E-Mail</th>';
+			  $retStr = $retStr . $html_th . 'Telefon</th>';
+			  $retStr = $retStr . $html_th . 'Status</th>';
+			  $retStr = $retStr . '</tr>';
+			}
 
+		    if( $order_data['status'] == "completed" ) {
 
-          if( $order_data['status'] == "completed" ) {
+			  // add order item data
+			  $retStr = $retStr . '<tr>';
+			  $retStr = $retStr . $html_td . pdf_download_link($invoice_number, $order_data['id']) . '</td>';
+			  $retStr = $retStr . $html_td . $order_data['billing']['first_name'] . ' ' . $order_data['billing']['last_name'] . '</td>';
+			  $retStr = $retStr . $html_td . $item_values['quantity'] . '</td>';
 
-            // add order item data
-            $retStr = $retStr . '<tr>';
-            $retStr = $retStr . $html_td . pdf_download_link($invoice_number, $order_data['id']) . '</td>';
-            $retStr = $retStr . $html_td . $order_data['billing']['first_name'] . ' ' . $order_data['billing']['last_name'] . '</td>';
-            $retStr = $retStr . $html_td . $item_values['quantity'] . '</td>';
+			  // add custom field data
+			  foreach( $item_values->get_meta_data('') as $meta_data) {
+			    if( !str_starts_with($meta_data->key, '_') ) {
+				  $retStr = $retStr . $html_td . $meta_data->value . '</td>';
+				}
+			  }
 
-            // add custom field data
-            foreach( $item_values->get_meta_data('') as $meta_data) {
-              if( !str_starts_with($meta_data->key, '_') ) {
-                $retStr = $retStr . $html_td . $meta_data->value . '</td>';
-              }
-            }
+			  $retStr = $retStr . $html_td . '<a href="mailto:' . $order_data['billing']['email'] . '">' . $order_data['billing']['email'] . '</a></td>';
+			  $retStr = $retStr . $html_td . '<a href="tel:' . $order_data['billing']['phone'] . '">' . $order_data['billing']['phone'] . '</a></td>';
+			  $retStr = $retStr . $html_td . $order_data['status'] . '</td>';
 
-            $retStr = $retStr . $html_td . '<a href="mailto:' . $order_data['billing']['email'] . '">' . $order_data['billing']['email'] . '</a></td>';
-            $retStr = $retStr . $html_td . '<a href="tel:' . $order_data['billing']['phone'] . '">' . $order_data['billing']['phone'] . '</a></td>';
-            $retStr = $retStr . $html_td . $order_data['status'] . '</td>';
-
-            $retStr = $retStr . '</tr>';
-          }
-        }
+			  $retStr = $retStr . '</tr>';
+			}
+		  }
+		}
       }
     }
     $retStr = $retStr . '</table>';
@@ -139,6 +154,7 @@ function func_list_orders_summary( $atts ) {
   $retStr = '';
   $html_th = '<td style="border:solid;padding:4px;font-size:small;font-size:0.8em;">';
   $html_td = '<td style="border:solid;padding:4px;white-space: nowrap;font-size:0.8em;">';
+  $five_months = 5 * 30 * 24 * 60 * 60; // 5 months in seconds
 
   // get all products
   $products = wc_get_products( array('numberposts' => -1) );
@@ -174,23 +190,35 @@ function func_list_orders_summary( $atts ) {
     foreach( $orders as $order ) {
       $order_t = $order;
 
-      // loop through all order items of current order
-      foreach ($order->get_items() as $item_key => $item_values) {
+	  // get order creation timestamp
+	  $date_created = $order->get_date_created();
+	  $date_created_timestamp = $date_created->getTimestamp();
 
-        // get product id of current order
-        $product_id = $item_values->get_product_id();
+	  // get current timestamp of "now"
+	  $now = new WC_DateTime(); 
+      $now->setTimezone( $date_created->getTimezone() );
+      $now_timestamp = $now->getTimestamp();
 
-        // check if product id was selected by user
-        if( $product_id == $product->get_id() ) {
+	  if( ($now_timestamp - $five_months) < $date_created_timestamp ) 
+	  {	
+        // loop through all order items of current order
+        foreach ($order->get_items() as $item_key => $item_values) {
 
-          // get order data of current order
-          $order_data = $order->get_data();
+          // get product id of current order
+          $product_id = $item_values->get_product_id();
 
-          if( $order_data['status'] == "completed" ) {
-            $quantity = $quantity + $item_values['quantity'];
-            $total = $total + $item_values['total'];
+          // check if product id was selected by user
+          if( $product_id == $product->get_id() ) {
+
+            // get order data of current order
+            $order_data = $order->get_data();
+
+            if( $order_data['status'] == "completed" ) {
+              $quantity = $quantity + $item_values['quantity'];
+              $total = $total + $item_values['total'];
+            }
           }
-        }
+	    }
       }
     }
 
@@ -199,7 +227,12 @@ function func_list_orders_summary( $atts ) {
     $retStr = $retStr . $html_td . $product_data['name'] . '</td>';
     $retStr = $retStr . $html_td . $quantity . '</td>';
     $retStr = $retStr . $html_td . $product_data['stock_quantity'] . '</td>';
-    $retStr = $retStr . $html_td . number_format(100 * $quantity / $product_data['stock_quantity'], 1, '.', '') . ' %</td>';
+	if( $product_data['stock_quantity'] != 0 ) {
+        $retStr = $retStr . $html_td . number_format(100 * $quantity / ($quantity + $product_data['stock_quantity']), 1, '.', '') . ' %</td>';
+	}
+	else {
+		$retStr = $retStr . $html_td . '-</td>';
+	}
     $retStr = $retStr . $html_td . $total . ' €</td>';
     $retStr = $retStr . '</tr>';
 
